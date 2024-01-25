77c84feee560344fa2c95669a05a6566203b0b1a
Bohdan Dmytriv <bdm.bdsm@gmail.com>
2024-01-25 15:32:15 +0200

    Add remaps that ease window motions

--- lua/bdmbdsm/remap.lua
+++ lua/bdmbdsm/remap.lua
@@ -1,1 +1,21 @@
+local keymap = vim.keymap.set
+
+-- Silent keymap option
+local opts = { silent = true }
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
 vim.g.mapleader = " "
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
