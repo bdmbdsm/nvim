a5cf9ab6cfa59029be43144ded0b53b8b869e7fb
Bohdan Dmytriv <bdm.bdsm@gmail.com>
2024-01-30 10:34:15 +0200

    Interesting remap

--- lua/bdmbdsm/remap.lua
+++ lua/bdmbdsm/remap.lua
@@ -19,3 +19,5 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("i", "jk", "<ESC>", opts)
